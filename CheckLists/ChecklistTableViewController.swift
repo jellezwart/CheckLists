4db47080e7938185d619a985e55f56f084d288b5
Jelle Zwart <jelle.zwart@icloud.com>
2020-03-01 09:20:14 +0100

    Changing the basic View controller to TableViewController

--- CheckLists/ChecklistTableViewController.swift
+++ CheckLists/ChecklistTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ChecklistViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
